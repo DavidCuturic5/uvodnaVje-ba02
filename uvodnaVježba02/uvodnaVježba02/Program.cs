d201c6e334cf44e5cda0bf7a5ede6063280bdc89
DavidCuturic5 <david.cuturic5@gmail.com>
2024-09-17 18:28:00 +0200

    Add files via upload

---
+++ uvodnaVježba02/uvodnaVježba02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVježba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double d, c, x;
+
+            a = 10;
+            b = 7;
+            c = 12.5;
+            d = 16.7;
+
+            x = (a + b + c + d) / 4;
+
+            string ispis = "Prosjecna vrijednost od brojeva " + a + ", " + b + ", " + c + " i " + d+""+ " je " + x;
+            Console.WriteLine( ispis );
+            Console.ReadKey ();
+        }
+    }
+}
